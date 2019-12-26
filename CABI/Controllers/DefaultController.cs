3c54774e6c8aea5f8ef977b651a8ed45afd79361
David Zheng <nmdigboy@hotmail.com>
2019-12-27 01:22:57 +0800

    初始化察必项目

---
+++ CABI/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CABI.Controllers
+{   
+
+    public class DefaultController : ApiController
+    {
+        [HttpGet,Route("test")]
+        public IHttpActionResult test()
+        {
+            return Ok("哈哈");
+        }
+    }
+}
