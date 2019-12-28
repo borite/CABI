29f3840c2966ecea9b84390920631fdef7c18e72
David Zheng <nmdigboy@hotmail.com>
2019-12-29 00:38:15 +0800

    加入产品详情DTO

--- CABIProgram/DTO/ProductDTO.cs
+++ CABIProgram/DTO/ProductDTO.cs
@@ -7,16 +7,51 @@ namespace CABIProgram.DTO
 {
     public class ProductDTO
     {
+        /// <summary>
+        /// 产品ID
+        /// </summary>
+        public int ID { get; set; }
+        /// <summary>
+        /// 分类ID
+        /// </summary>
+        public int ThemeID { get; set; }
+        /// <summary>
+        /// 产品名字
+        /// </summary>
+        public string NewTitle { get; set; }
+        /// <summary>
+        /// 产品价格
+        /// </summary>
+        public Nullable<decimal> Price { get; set; }
 
-        public class productID
-        {
+        /// <summary>
+        /// 编辑器内容
+        /// </summary>
+        public string Contents { get; set; }
 
-            /// <summary>
-            /// 产品ID
-            /// </summary>
-          public int ID { get;set;}
+        /// <summary>
+        /// 产品颜色
+        /// </summary>
+        public string Color { get; set; }
 
-        }
+        /// <summary>
+        /// 产品尺寸
+        /// </summary>
+        public string SizeInfo { get; set; }
+
+        /// <summary>
+        /// 产品适用场景
+        /// </summary>
+        public string Scene { get; set; }
+        /// <summary>
+        /// 滚动图片
+        /// </summary>
+        public string ListImg { get; set; }
+
+        /// <summary>
+        /// 面料图片
+        /// </summary>
+        public string ImgList { get; set; }
 
 
     }
